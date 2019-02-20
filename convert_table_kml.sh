9a10910c1545c6b26d0334d97a26e1fce92ccaf5
Zeta Sagittarii <zeta@sagittarii.fr>
2019-02-20 23:37:22 +0100

    Change output format to kmz with embedded image, and output only modified SUC, to match the current map format

--- convert_table_kml.sh
+++ convert_table_kml.sh
@@ -5,93 +5,48 @@ curl "https://docs.google.com/spreadsheets/d/1N7CjkpUITPmj3ObOvTZnH8AtgmsinqOJ_Z
 
 
 # Remove headers from data.tsv
-sed -i data.tsv -e "1,8d"
+sed -i data.tsv -e "1,6d"
 
 #cat data.tsv | awk -F'\t' '{print $2, $5, $4, $9, $8}' 
 #cat data.tsv | awk -F'\t' '{print "<name>",$2,"</name><description>","$5," CCS / ",$4," total</description><Point>",$9,",",$8,"</Point>"}' 
-cat data.tsv | awk -F'\t' '{print "<Placemark><name>",$2,"</name><description>",$5," CCS / ",$4," total</description><styleUrl>STYLE</styleUrl><Point><coordinates>",$9,",0</coordinates></Point></Placemark>"}' | sed -e "s/\([0-9.\-]\+\) *, *\([0-9.\-]\+\) *, *0/\2, \1, 0/"> data.kml
+cat data.tsv | awk -F'\t' '{print "<Placemark><name>",$3,"</name><description><![CDATA[CCS: ",$5,"<br>Address: ",$3,"<br>Country: ",$1,"]]></description><styleUrl>STYLE</styleUrl><ExtendedData><Data name=\"CCS\"><value>",$5,"</value></Data><Data name=\"Address\"><value>",$3,"</value></Data><Data name=\"Country\"><value>",$1,"</value></Data></ExtendedData><Point><coordinates>",$9,",0</coordinates></Point></Placemark>"}' | sed -e "s/\([0-9.\-]\+\) *, *\([0-9.\-]\+\) *, *0/\2, \1, 0/"> data.kml
 
-cat << EOF >> SUC_CCS.kml
+rm -f doc.kml
+cat << EOF >> doc.kml
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
-    <Style id="icon-1899-0F9D58-normal">
+    <name>TESLA CCS SUPERCHARGERS</name>
+    <description>Tesla Supercharger with CCS for Model 3</description>
+    <Style id="icon-ci-1-labelson">
       <IconStyle>
-        <color>ff589d0f</color>
-        <scale>1</scale>
+        <scale>1.1</scale>
         <Icon>
-          <href>http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png</href>
+          <href>images/icon-1.png</href>
         </Icon>
-        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
       </IconStyle>
-      <LabelStyle>
-        <scale>0</scale>
-      </LabelStyle>
     </Style>
-    <Style id="icon-1899-0F9D58-highlight">
-      <IconStyle>
-        <color>ff589d0f</color>
-        <scale>1</scale>
-        <Icon>
-          <href>http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png</href>
-        </Icon>
-        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
-      </IconStyle>
-      <LabelStyle>
-        <scale>1</scale>
-      </LabelStyle>
-    </Style>
-    <StyleMap id="icon-1899-0F9D58">
-      <Pair>
-        <key>normal</key>
-        <styleUrl>#icon-1899-0F9D58-normal</styleUrl>
-      </Pair>
-      <Pair>
-        <key>highlight</key>
-        <styleUrl>#icon-1899-0F9D58-highlight</styleUrl>
-      </Pair>
-    </StyleMap>
- <Style id="icon-1899-C2185B-normal">
-      <IconStyle>
-        <color>ff5b18c2</color>
-        <scale>1</scale>
-        <Icon>
-          <href>http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png</href>
-        </Icon>
-        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
-      </IconStyle>
-      <LabelStyle>
-        <scale>0</scale>
-      </LabelStyle>
-    </Style>
-    <Style id="icon-1899-C2185B-highlight">
-      <IconStyle>
-        <color>ff5b18c2</color>
-        <scale>1</scale>
-        <Icon>
-          <href>http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png</href>
-        </Icon>
-        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
-      </IconStyle>
-      <LabelStyle>
-        <scale>1</scale>
-      </LabelStyle>
-    </Style>
-   <StyleMap id="icon-1899-C2185B">
-      <Pair>
-        <key>normal</key>
-        <styleUrl>#icon-1899-C2185B-normal</styleUrl>
-      </Pair>
-      <Pair>
-        <key>highlight</key>
-        <styleUrl>#icon-1899-C2185B-highlight</styleUrl>
-      </Pair>
-    </StyleMap>
+    <Folder>
+      <name>SUPERCHARGER CCS</name>
 EOF
 
 
-grep "> *0\? *CCS" data.kml | sed -e "s/> *CCS/> 0 CCS/" -e "s/STYLE/#icon-1899-C2185B/" >> SUC_CCS.kml
-grep -v "> *0\? *CCS" data.kml | sed -e "s/STYLE/#icon-1899-0F9D58/" >> SUC_CCS.kml
+#grep "> *0\? *CCS" data.kml | sed -e "s/> *CCS/> 0 CCS/" -e "s/STYLE/#icon-1899-C2185B/" -e "s/>/\n>/g" >> doc.kml
+grep -v "CCS: *0\? *<br" data.kml | sed -e "s/STYLE/#icon-ci-1-labelson/" >> doc.kml
 
-echo '</Document></kml>' >> SUC_CCS.kml
 
+
+cat << EOF >> doc.kml
+</Folder>
+</Document>
+</kml>
+EOF
+
+
+zip 'TESLA CCS SUPERCHARGERS.kmz' doc.kml images/*
+
+
+
+
+
+#curl "https://docs.google.com/spreadsheets/d/1N7CjkpUITPmj3ObOvTZnH8AtgmsinqOJ_ZR2MNJFnVI/export?format=tsv&id=1N7CjkpUITPmj3ObOvTZnH8AtgmsinqOJ_ZR2MNJFnVI&gid=770972283" -o tesla_data.tsv
